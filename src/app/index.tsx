8127ac414576c494634bb5f06cdd355c0038e82d
novalfh <annaufal1521@gmail.com>
2026-09-25 04:34:49 +0800

    installed vector-icon

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { Text, View } from "react-native";
 
 
@@ -5,6 +6,7 @@ export default function Index() {
   return (
     <View >
       <Text className="bg-blue-500">Edit src/app/index.tsx to edit this screen.</Text>
+      <AntDesign name="account-book" size={24} color="black" />
     </View>
   );
 }
